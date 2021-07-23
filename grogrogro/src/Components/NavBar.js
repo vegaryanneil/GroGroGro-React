ef72dcbfe0903baede645da1aad42f11e73f71d0
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-07-23 17:07:53 -0400

    Installed Tailwinds CSS, started NavBar

---
+++ grogrogro/src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <Link to='/' className='pl-8'>
+                GroGroGro
+            </Link>
+            <div className="px-4 cursor-pointer md:hidden">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
