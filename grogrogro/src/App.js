49bbcc02c148017de7c536c417069028dadc592e
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-07-24 23:09:19 -0400

    Added Button and Landing Text

--- grogrogro/src/App.js
+++ grogrogro/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GlobalStyle from "./Components/GlobalStyle";
+import Hero from "./Components/Hero";
 import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <>  
-    <GlobalStyle/>
-    <NavBar/>
+    <GlobalStyle />
+    <NavBar />
+    <Hero />
     </>
   );
 }
